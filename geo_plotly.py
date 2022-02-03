af2df6949d7dcb866124f1c84ce0fb7f57a90fd1
mahdi_Tonismagi14 <you@example.com>
2022-02-03 12:59:12 +0200

    made an od series to use in folium or px.line_mapbox

--- geo_plotly.py
+++ geo_plotly.py
@@ -1,67 +1,12 @@
-# https://plotly.com/python/lines-on-maps/
-import plotly.graph_objects as go
 import pandas as pd
 
-city = pd.read_csv('points.csv')
-city.head()
+estonia = pd.read_csv("Estonia_od_serie.csv")
 
-od = pd.read_csv('od.csv')
-od.head()
+import plotly.express as px
 
-fig = go.Figure()
+fig = px.line_mapbox(estonia, lat="lat", lon="lon", color="locations", zoom=3, height=300)
 
-
-lons = []
-lats = []
-import numpy as np
-lons = np.empty(3 * len(od))
-lons[::3] = od['start_lon']
-lons[1::3] = od['end_lon']
-lons[2::3] = None
-lats = np.empty(3 * len(od))
-lats[::3] = od['start_lat']
-lats[1::3] = od['end_lat']
-lats[2::3] = None
-
-
-fig.add_trace(
-    go.Scattergeo(
-        lon = lons,
-        lat = lats,
-        mode = 'lines',
-        line = dict(width = 1,color = 'red'),
-        opacity = 0.25
-    )
-)
-
-
-fig.add_trace(go.Scattergeo(
-    lon = city['lng'],
-    lat = city['lat'],
-    hoverinfo = 'text',
-    text = city['name'],
-    mode = 'markers',
-    marker = dict(
-        size = 2,
-        color = 'rgb(255, 0, 0)',
-        line = dict(
-            width = 3,
-            color = 'rgba(68, 68, 68, 0)'
-        )
-    )))
-
-# fig.update_layout(
-#     title_text = 'March 2018 Estonia Origin Destination Matrice <br>',
-#     showlegend = False,
-#     geo = go.layout.Geo(
-#         scope = 'europe',
-#         # projection_type = 'van der grinten3',
-#         showland = True,
-#         landcolor = 'rgb(243, 243, 243)',
-#         countrycolor = 'rgb(204, 204, 204)',
-#     ),
-#     height=1200,
-#     width=1600,
-# )
+fig.update_layout(mapbox_style="stamen-terrain", mapbox_zoom=6, mapbox_center_lat = 58,
+    margin={"r":0,"t":20,"l":0,"b":0},height=1200, width=1600,)
 
 fig.show()
