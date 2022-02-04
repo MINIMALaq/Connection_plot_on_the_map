63add4b22ad629a0f196474e6bb6be4cc34e48fb
mahdi_Tonismagi14 <you@example.com>
2022-02-04 11:47:54 +0200

    Tallinn city OD connections on Plotly map is ready.

--- README.md
+++ README.md
@@ -5,7 +5,10 @@ Visualizing Sankey plot on a map with python.
 I downloaded one of the Origin Destination files from [here](https://mobilitylab.ut.ee/OD/) and made it ready to use woth 4326 projection. 
 
 
-<br>It is not a Sakey plot yet, but to see the connection plot with plotly you can run the code like this:<br>
+<br>It is not a Sakey plot yet, but to see the connection plot with plotly. <br>
+I made a HTML version on Tallinn notebook so you can see the results before running the code. <br>
+
+You can run the code like this:<br>
 `pip install -r requirements.txt` <br>
 `python geo_plotly.py`
 
--- geo_plotly.py
+++ geo_plotly.py
@@ -1,12 +1,17 @@
 import pandas as pd
-
-estonia = pd.read_csv("Estonia_od_serie.csv")
-
 import plotly.express as px
 
-fig = px.line_mapbox(estonia, lat="lat", lon="lon", color="locations", zoom=3, height=300)
 
-fig.update_layout(mapbox_style="stamen-terrain", mapbox_zoom=6, mapbox_center_lat = 58,
+df = pd.read_csv("Tallinn_od_serie.csv")
+
+# You can also visualize Estonia simplified od
+# df = pd.read_csv("Estonia_od_serie.csv")
+
+
+
+fig = px.line_mapbox(df, lat="lat", lon="lon", color="locations", zoom=15, height=300)
+
+fig.update_layout(mapbox_style="stamen-terrain", mapbox_zoom=6, mapbox_center_lat = 59.3550,
     margin={"r":0,"t":20,"l":0,"b":0},height=1200, width=1600,)
 
 fig.show()
